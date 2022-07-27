41f6522ab326dd76fd84df61024a8403d79bbc65
JiHyeok Kim <jkl410907@npixel.co.kr>
2022-07-27 18:36:39 +0900

    Implement : Lobby

--- Assets/Scripts/LobbyPanel.cs
+++ Assets/Scripts/LobbyPanel.cs
@@ -4,32 +4,129 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
+
+
 public class LobbyPanel : MonoBehaviourPunCallbacks
 {
     #region UI PARAM
 
-    public InputField PlayerNameInput;
+    [Header("Login Panel")]
+    public GameObject LoginPanel;
+
+    public TMP_InputField PlayerNameInput;
+
+    [Header("Selection Panel")]
+    public GameObject SelectionPanel;
+
+    public GameObject RoomListPanel;
+    public GameObject RoomListEntryPrefeb;
+
+    [Header("Create Room Panel")]
+    public GameObject CreateRoomPanel;
+
+    public TMP_InputField RoomNameInput;
+    public TMP_InputField MaxPlayersInput;
+
+    [Header("InRoom Panel")]
+    public GameObject InRoomPanel;
+
+    public GameObject PlayerListPanel;
+    public GameObject PlayerListEntryPrefeb;
+    public GameObject ChatPanel;
+    public GameObject StatePanel;
+    public GameObject MapPanel;
+    public TMP_InputField ChatInput;
+    public Button ReadyStartButton;
+
+    [Header("Loading Panel")]
+    public GameObject LoadingPanel;
 
     #endregion
 
     #region LOBBY PARAM
 
-    public bool isOffline = true;
+    public bool isOffline = false;
     public string playerName;
-    public string sceneName = "LookA_Scene";
+    public string roomName;
+    private string sceneName = "SampleScene";
+    public byte maxPlayers = 16;
+    public RoomOptions roomOptions;
 
     #endregion
 
     void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
+        //Screen.SetResolution(960, 540, false);
+        SetActivePanel(LoginPanel.name);
     }
 
     #region PUN CALLBACKS
 
+    public void OnConnect()
+    {
+        if (isOffline)
+        {
+            PhotonNetwork.Disconnect();
+
+            PhotonNetwork.OfflineMode = isOffline;
+        }
+        else
+        {
+            SetActivePanel(SelectionPanel.name);
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
     public override void OnConnectedToMaster()
     {
-        
+        Debug.Log(PhotonNetwork.NetworkClientState.ToString());
+        if (isOffline)
+        {
+            PhotonNetwork.JoinRandomOrCreateRoom();
+        }
+        else
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        SetActivePanel(LoginPanel.name);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log(PhotonNetwork.NetworkClientState.ToString());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NetworkClientState.ToString());
+        if (isOffline)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.CurrentRoom.IsVisible = false;
+
+            PhotonNetwork.LoadLevel(sceneName);
+        }
+        else
+        {
+            SetActivePanel(InRoomPanel.name);
+        }
+    }
+
+    public override void OnCreatedRoom()
+    {
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null);
     }
 
     #endregion
@@ -39,16 +136,31 @@ public class LobbyPanel : MonoBehaviourPunCallbacks
     public void OnLoginButtonClicked()
     {
         isOffline = false;
+        playerName = PlayerNameInput.text;
+
+        if (playerName == "" || playerName.Length > 12)
+        {
+            PlayerNameInput.text = "";
+            PlayerNameInput.placeholder.GetComponent<TMP_Text>().text = " Please Name(Length 1 ~ 12)";
+        }
+        else
+        {
+            OnConnect();
+        }
     }
+
     public void OnOfflineButtonClicked()
     {
+
         isOffline = true;
+        OnConnect();
     }
 
     public void OnCreateRoomButtonClicked()
     {
-
+        SetActivePanel(CreateRoomPanel.name);
     }
+
     public void OnJoinRandomRoomButtonClicked()
     {
         PhotonNetwork.JoinRandomRoom();
@@ -56,27 +168,86 @@ public class LobbyPanel : MonoBehaviourPunCallbacks
 
     public void OnBackButtonClicked()
     {
-        PhotonNetwork.LeaveLobby();
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+
+        PhotonNetwork.Disconnect();
+        Debug.Log(PhotonNetwork.NetworkClientState.ToString());
     }
 
-    public void OnLeaveRoomButtonClicked()
+    public void OnCreateButtonClicked()
     {
+        if (RoomNameInput.text == "" || RoomNameInput.text.Length > 20)
+        {
+            RoomNameInput.text = "";
+            RoomNameInput.placeholder.GetComponent<TMP_Text>().text = " Please Name(Length 1 ~ 20)";
+        }
+        else if (Int32.Parse(MaxPlayersInput.text) <= 0 || Int32.Parse(MaxPlayersInput.text) > 16 || IsDigit(MaxPlayersInput.text) == false)
+        {
+            MaxPlayersInput.text = "";
+            MaxPlayersInput.placeholder.GetComponent<TMP_Text>().text = " Please Number(limit 1 ~ 16)";
+        }
+        else
+        {
+            roomName = RoomNameInput.text;
+            maxPlayers = Byte.Parse(MaxPlayersInput.text);
+            roomOptions = new RoomOptions{ MaxPlayers = maxPlayers };
+
+            PhotonNetwork.CreateRoom(roomName, roomOptions);
+            SetActivePanel(InRoomPanel.name);
+        }
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        SetActivePanel(SelectionPanel.name);
+    }
+
+    public void OnReadyStartButtonClicked()
+    {
+        // all ready state
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.CurrentRoom.IsVisible = false;
+
+            PhotonNetwork.LoadLevel(sceneName);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnLeaveButtonClicked()
+    {
+        SetActivePanel(SelectionPanel.name);
         PhotonNetwork.LeaveRoom();
     }
 
-    public void OnReadyButtonClicked()
+    private void SetActivePanel(string activePanel)
     {
-
-    }
-
-    public void OnStartButtonClicked()
-    {
-        PhotonNetwork.CurrentRoom.IsOpen = false;
-        PhotonNetwork.CurrentRoom.IsVisible = false;
-
-        PhotonNetwork.LoadLevel(sceneName);
+        LoginPanel.SetActive(activePanel.Equals(LoginPanel.name));
+        SelectionPanel.SetActive(activePanel.Equals(SelectionPanel.name));
+        CreateRoomPanel.SetActive(activePanel.Equals(CreateRoomPanel.name));
+        InRoomPanel.SetActive(activePanel.Equals(InRoomPanel.name));
+        LoadingPanel.SetActive(activePanel.Equals(LoadingPanel.name));
     }
 
     #endregion
 
+    public bool IsDigit(string str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] < 48 || str[i] > 57)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
