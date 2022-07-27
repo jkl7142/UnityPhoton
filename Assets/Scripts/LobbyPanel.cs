054d77625ec4b08b72ea91e5c3dda93656d8247a
JiHyeok Kim <jkl410907@npixel.co.kr>
2022-07-27 19:01:12 +0900

    Implement : room, player list

--- Assets/Scripts/LobbyPanel.cs
+++ Assets/Scripts/LobbyPanel.cs
@@ -49,11 +49,18 @@ public class LobbyPanel : MonoBehaviourPunCallbacks
     #region LOBBY PARAM
 
     public bool isOffline = false;
+
     public string playerName;
+    
     public string roomName;
-    private string sceneName = "SampleScene";
     public byte maxPlayers = 16;
     public RoomOptions roomOptions;
+
+    private string sceneName = "SampleScene";
+
+    private Dictionary<string, RoomInfo> cachedRoomList;
+    private Dictionary<string, GameObject> roomListEntries;
+    private Dictionary<int, GameObject> playerListEntries;
 
     #endregion
 
@@ -61,6 +68,10 @@ public class LobbyPanel : MonoBehaviourPunCallbacks
     {
         PhotonNetwork.AutomaticallySyncScene = true;
         //Screen.SetResolution(960, 540, false);
+
+        cachedRoomList = new Dictionary<string, RoomInfo>();
+        roomListEntries = new Dictionary<string, GameObject>();
+
         SetActivePanel(LoginPanel.name);
     }
 
@@ -102,6 +113,28 @@ public class LobbyPanel : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log(PhotonNetwork.NetworkClientState.ToString());
+
+        if (isOffline == false)
+        {
+            cachedRoomList.Clear();
+            // Å¬¸®¾î ·ë ¸®½ºÆ® ºä
+        }
+    }
+
+    public override void OnLeftLobby()
+    {
+        if (isOffline == false)
+        {
+            cachedRoomList.Clear();
+            // Å¬¸®¾î ·ë ¸®½ºÆ® ºä
+        }
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        // Å¬¸®¾î ·ë ¸®½ºÆ® ºä
+        // ¾÷µ¥ÀÌÆ® ·ë ¸®½ºÆ® Ä³½¬
+        // ¾÷µ¥ÀÌÆ® ·ë ¸®½ºÆ®ºä
     }
 
     public override void OnJoinedRoom()
@@ -116,8 +149,21 @@ public class LobbyPanel : MonoBehaviourPunCallbacks
         }
         else
         {
+            cachedRoomList.Clear();
+
             SetActivePanel(InRoomPanel.name);
         }
+    }
+
+    public override void OnLeftRoom()
+    {
+        foreach (GameObject entry in playerListEntries.Values)
+        {
+            Destroy(entry.gameObject);
+        }
+
+        playerListEntries.Clear();
+        playerListEntries = null;
     }
 
     public override void OnCreatedRoom()
